063d0754865e0378541159facd4260ba469e2b8e
Charles Barkley <dpb522@mail.missouri.edu>
2020-05-08 10:03:00 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.test = this.test.bind(this)
+    this.otherTest = this.otherTest.bind(this)
+  }
+
+  test(){
+    if(window.location.href.match(/code=([^]*)/)){
+      return fetch('/code', {
+        method: 'POST', 
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({code: window.location.href.match(/code=([^]*)/)})
+      })
+    } else {
+      return fetch('/api/hey')
+      .then(res=>res.json())
+      .then(res=>{
+        window.location = res
+      })
+      .then(res=>{
+        
+      })
+    }
+  }
+
+  refreshToken(){
+    return fetch('/callback')
+  }
+
+  otherTest(e){
+    e.preventDefault()
+
+    return fetch('/app/authorize')
+      .then(res=>{
+        //console.log(res)
+        return res.json()
+      }).then(res=>{
+        window.location = res.url
+        
+        
+      })
+      
+      
+  }
+
+
+  runMe(){
+    if(window.location.href.match(/code=([^]*)/)){
+      let code = window.location.href.match(/code=([^]*)/)[1]
+
+      fetch(`/app/callback/?code=${code}`)
+        .then(res=>{
+          console.log(res)
+        })
+    }
+  }
+  
+
+  runOnLoad(){
+    const hash = window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce(function (initial, item) {
+      if (item) {
+        var parts = item.split('=');
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+      }
+      return initial;
+        }, {});
+      window.location.hash = '';
+
+      if (hash.access_token) {
+        fetch('/app/myendpoint', {headers: {"Authorization": `Bearer ${hash.access_token}`}})
+          .then(res=>{
+            console.log(res)
+          })
+      }
+  }
+
+  componentDidMount(){
+    this.runMe()
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Hello!</h1>
+        <button onClick={this.otherTest}>click to log in</button>
+        <button onClick={this.refreshToken}>click to refresh token</button>
+      </div>
+    );
+  }
 }
 
 export default App;
